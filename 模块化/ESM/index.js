1ffd888e687bda518538932df569d29108749098
rayhomie <1572801584@qq.com>
2021-05-11 14:43:54 +0800

    ESM

--- 模块化/ESM/index.js
+++ 模块化/ESM/index.js
@@ -1,11 +1,17 @@
-// import { Name, Age, SayHello } from './modules/module1.js';
-// console.log(Name, Age);
-// SayHello('leihao')
+import { name, person, Age, SayHello, modify } from './modules/module1.js';
 
-let flag = true;
-if (flag) {//运行时
-  const promise = import('./modules/module1.js')
-  promise.then((res) => {
-    console.log(res);
-  })
-}
+SayHello('leihao')
+modify()
+setTimeout(() => {
+  console.log(name);
+}, 2000)
+console.log(name);
+console.log(person);
+person.name = 'green'
+// let flag = true;
+// if (flag) {//运行时
+//   const promise = import('./modules/module1.js')
+//   promise.then((res) => {
+//     console.log(res);
+//   })
+// }
--- 模块化/ESM/modules/module1.js
+++ 模块化/ESM/modules/module1.js
@@ -1,6 +1,17 @@
-const name = 'rayhomie'
-const age = 20
-const sayHello = function (name) {
+let name = 'rayhomie'
+let age = 20
+
+let person = {
+  name: 'John',
+}
+let sayHello = function (name) {
   console.log('你好', name);
 }
-export { name as Name, age as Age, sayHello as SayHello}
+let modify = function () {
+  age = 10
+}
+setTimeout(() => {
+  name = 'leihao'
+  console.log(person);
+}, 1000)
+export { name, person, age as Age, sayHello as SayHello, modify }
